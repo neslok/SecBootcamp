0abbfe3343d3727c06a04c2bad213ed0081ab2e9
keith.olsen <keith.r.olsen@gmail.com>
2021-01-21 20:31:12 -0700

    Added clean up and state check scripts
    SecCleanup will delete blueprints and applications (ALL APPLICATIONS).
    
    SecBTCMP_State will report the state of the applications (provisioning/running/deleting/error) so you can easily check after provisioning, and address any errors without having to check each cluster individually.

---
+++ SecBTCMP_State.py
@@ -0,0 +1,73 @@
+import requests
+import json
+from http.client import HTTPSConnection
+from base64 import b64encode
+import re
+import time
+import csv
+
+#
+# Defines Prism Central variables - update these with your HPOC details (uncomment for single cluster config)
+
+# pcUserID = 'admin'
+# pcPassword = 'nx2Tech123!'
+# #prisCentIP = '10.38.3.73'
+# prisCentIP = input("Enter Prism Central IP: ")
+
+clusterdeets = '/Users/keith.olsen/Downloads/clusters.csv' # Path to csv file with cluster info
+
+# Reads in cluster details from csv file
+
+with open(clusterdeets) as csvfile:
+    readCSV = csv.reader(csvfile, delimiter=',')
+    for row in readCSV:
+        prisCentIP = (row[0])
+        #print(prisCentIP)
+        pcUserID = (row[1])
+        #print(pcUserID)
+        pcPassword = (row[2])
+        #print(pcPassword)
+
+
+
+        # This sets up the https connection
+
+        c = HTTPSConnection(prisCentIP)
+
+
+        # # Creates encoded Authorization value
+
+        userpass = pcUserID + ":" + pcPassword
+        buserAndPass = b64encode(userpass.encode("ascii"))
+        authKey = (buserAndPass.decode("ascii"))
+
+        headers = {
+            'Content-Type': "application/json",
+            'Authorization': "Basic " + authKey,
+            'cache-control': "no-cache"
+        }
+
+        # # Defines base url for API calls
+
+        baseurl = "https://" + prisCentIP + ":9440/api/nutanix/v3/"
+
+        # Get APP list
+
+        APPpayload = {'kind': 'app'}
+
+        json_APPpayload = json.dumps(APPpayload)
+
+        APPlist = requests.request("POST", baseurl + "apps/list", headers=headers, data=json_APPpayload, verify=False).json()
+        json_APPlist = json.dumps(APPlist)
+
+        #print(json_APPlist)
+
+        #print(APPlist)
+        print(prisCentIP)
+        for each in APPlist['entities']:
+            APPUuid = (each['status']['uuid'])
+            APPName = (each['status']['name'])
+            APPStat = (each['status']['state'])
+
+
+            print(APPName + " " + APPStat)
